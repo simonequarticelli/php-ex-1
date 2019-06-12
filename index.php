0b0ed515c0b574304498175c9baae6fc3b1c321c
simonequarticelli <quarti87@gmail.com>
2019-06-12 15:03:21 +0200

    init php-ex-1

---
+++ index.php
@@ -0,0 +1,13 @@
+<!-- creare una variabile con del testo (es: lorem ipsum).
+Leggere dal parametro GET un parola da censurare e
+stampare a schermo un paragrafo contenente il testo
+con 3 * (3 caratteri di asterisco) al posto della badword. -->
+
+<html>
+  <head>
+    <title>php-ex-1</title>
+  </head>
+  <body>
+    <?php echo "ciao"; ?>
+  </body>
+</html>

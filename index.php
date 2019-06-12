cd864e05615f0ea18201f7cbd7afb6f564bc08d9
simonequarticelli <quarti87@gmail.com>
2019-06-12 17:39:42 +0200

    ex php-ex-1 complete

--- index.php
+++ index.php
@@ -8,6 +8,17 @@ con 3 * (3 caratteri di asterisco) al posto della badword. -->
     <title>php-ex-1</title>
   </head>
   <body>
-    <?php echo "ciao"; ?>
+    <?php
+    // echo "ciao";
+    //creo variabile con il testo
+    $testo = 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.';
+    //var_dump($testo);
+    //GET per leggere parola inserita
+    $parola_vietata = $_GET["parola"];
+    // var_dump($parola_vietata);
+    //censuro la parola inserita con ***
+    $testo_mod = str_replace($parola_vietata, '***', $testo);
+    echo $testo_mod;
+    ?>
   </body>
 </html>
